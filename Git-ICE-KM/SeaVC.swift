505ed56ee10549dca2c18db10137cd4e6e1de603
Student <s525909@ch140010m.nwmissouri.edu>
2016-11-14 16:45:08 -0600

    Added SeaVC, renamed FirstViewController and SecondViewController as EhVC and BeeVC.

---
+++ Git-ICE-KM/SeaVC.swift
@@ -0,0 +1,13 @@
+//
+//  SeaVC.swift
+//  Git-ICE-KM
+//
+//  Created by Mukka,Kranthi Kumar on 11/14/16.
+//  Copyright © 2016 Mukka,Kranthi Kumar. All rights reserved.
+//
+
+import UIKit
+
+class SeaVC: UIViewController {
+
+}
